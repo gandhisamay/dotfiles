fe4cddeb90d1a55736ff6070a74c25ac5e5480ef
Samay Gandhi <gandhisamay200@gmail.com>
2023-01-08 17:45:59 +0530

    feat(backup-script): commit msg passing via args
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   backup.sh
    	modified:   i3/.config/i3/config
    	modified:   neovim/.config/nvim/lua/user/gitsigns.lua
    	modified:   scripts/.config/scripts/toggle_monitor.sh
    
    no changes added to commit (use "git add" and/or "git commit -a")
    
    Regular Backup
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   backup.sh
    
    no changes added to commit (use "git add" and/or "git commit -a")
    
    Regular Backup
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   backup.sh
    
    no changes added to commit (use "git add" and/or "git commit -a")
    
    Regular Backup
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   backup.sh
    
    no changes added to commit (use "git add" and/or "git commit -a")
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   backup.sh
    
    no changes added to commit (use "git add" and/or "git commit -a")
    
    Regular backup
    
    On branch master
    Your branch is ahead of 'origin/master' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   backup.sh
    
    no changes added to commit (use "git add" and/or "git commit -a")
    
    Regular backup
    
    On branch master
    Your branch is ahead of 'origin/master' by 2 commits.
      (use "git push" to publish your local commits)
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   backup.sh
    
    no changes added to commit (use "git add" and/or "git commit -a")
    
    On branch master
    Your branch is ahead of 'origin/master' by 3 commits.
      (use "git push" to publish your local commits)
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   backup.sh
    
    no changes added to commit (use "git add" and/or "git commit -a")
    
    updated backup.sh script for args
    
    On branch master
    Your branch is ahead of 'origin/master' by 4 commits.
      (use "git push" to publish your local commits)
    
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git restore <file>..." to discard changes in working directory)
    	modified:   backup.sh
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- backup.sh
+++ backup.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-export DISPLAY=:0
 
-crontab -l > $HOME/dotfiles/crontabs
-
-error_exit()
-{
+error_exit(){
     notify-send -u critical " Backup Logs" "Error while performing regular backup."
     exit 1
 }
 
-main(){
-  CHANGES_NOT_STAGED="Changes not staged for commit"
-  CHANGES_TO_COMMIT="Changes to be committed"
-  GIT_STATUS=$(git status)
+export DISPLAY=:0
+crontab -l > $HOME/dotfiles/crontabs
 
-  if  grep -q "$CHANGES_NOT_STAGED" <<< "$GIT_STATUS"  ||  grep -q "$CHANGES_TO_COMMIT" <<< "$GIT_STATUS" ;then
-    git add . 
-    git commit -m "Regular backup" -m "$GIT_STATUS"
-    git push || error_exit
 
-    notify-send -u normal " Backup Logs" "Regular backup successfully completed."
-  fi
-}
+CHANGES_NOT_STAGED="Changes not staged for commit"
+CHANGES_TO_COMMIT="Changes to be committed"
+GIT_STATUS=$(git status)
 
-main
+if [ "$1" != "" ]; then
+  COMMIT_MESSAGE="$*" 
+else 
+  COMMIT_MESSAGE="Regular backup"
+fi 
+
+echo $COMMIT_MESSAGE
+
+if  grep -q "$CHANGES_NOT_STAGED" <<< "$GIT_STATUS"  ||  grep -q "$CHANGES_TO_COMMIT" <<< "$GIT_STATUS" ;then
+  git add . 
+  git commit -m "$COMMIT_MESSAGE" -m "$GIT_STATUS"
+  git push || error_exit
+
+  notify-send -u normal " Backup Logs" "Regular backup successfully completed."
+fi
+
--- scripts/.config/scripts/toggle_monitor.sh
+++ scripts/.config/scripts/toggle_monitor.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-CURRENT_MONITOR=$(xrandr --listactivemonitors)
+# Get the currently connected monitor numbers.
+CURRENT_MONITORS=$(xrandr --listactivemonitors)
 HDMI="HDMI-1-0"
 NOT_FOUND="not found"
 
-if grep -q "$HDMI" <<< "$CURRENT_MONITOR"; then
-  OUTPUT=$(xrandr --output eDP --auto)
-  if !(grep -q "$NOT_FOUND" <<< "$OUTPUT"); then
-    xrandr --output HDMI-1-0 --off
+if !(grep -q "1" <<< "$CURRENT_MONITORS"); then
+  if grep -q "$HDMI" <<< "$CURRENT_MONITORS"; then
+    OUTPUT=$(xrandr --output eDP --auto)
+    if !(grep -q "$NOT_FOUND" <<< "$OUTPUT"); then
+      xrandr --output HDMI-1-0 --off
+    fi
+  else 
+    OUTPUT=$(xrandr --output HDMI-1-0 --auto)
+    if !(grep -q "$NOT_FOUND" <<< "$OUTPUT"); then
+      xrandr --output eDP --off
+    fi
   fi
 else 
-  OUTPUT=$(xrandr --output HDMI-1-0 --auto)
-  if !(grep -q "$NOT_FOUND" <<< "$OUTPUT"); then
-    xrandr --output eDP --off
-  fi
+  notify-send -u normal " xrandr" "Failed to switch monitor, only 1 monitor active."
 fi
